3cd70e7d4c1972f7f6d8904ede5841a6ebd2dc22
Plenty Su <plenty.su@gmail.com>
2016-03-16 15:26:48 +0000

    Use selector contains method

--- fetnet.js
+++ fetnet.js
@@ -13,22 +13,20 @@ var push = new pushover({ token: pushover_config.token });
 request(target, function (error, response, body) {
     if (!error) {
         var $ = cheerio.load(body);
-        $('a[href$=".xlsx"]').each(
+        $('a[href$=".xlsx"]:contains("下載每月營運資訊")').each(
             function (i, e) {
-                if ("下載每月營運資訊" == $(e).text()) {
-                    var href = $(e).attr("href");
-                    var filename = path.basename(href);
-                    var whitelist = JSON.parse(fs.readFileSync("whitelist.json"));
-                    if (whitelist.fetnet.indexOf(filename) === -1) {
-                        var title = "Note";
-                        var message = `Far EasTone Telecom might update a new financial statement: <${filename}>`;
-                        for (var user of pushover_config.users) {
-                            push.send(user, title, message);
-                        }
-
-                        whitelist.fetnet.push(filename);
-                        fs.writeFileSync("whitelist.json", JSON.stringify(whitelist));
+                var href = $(e).attr("href");
+                var filename = path.basename(href);
+                var whitelist = JSON.parse(fs.readFileSync("whitelist.json"));
+                if (whitelist.fetnet.indexOf(filename) === -1) {
+                    var title = "Note";
+                    var message = `Far EasTone Telecom might update a new financial statement: <${filename}>`;
+                    for (var user of pushover_config.users) {
+                        push.send(user, title, message);
                     }
+
+                    whitelist.fetnet.push(filename);
+                    fs.writeFileSync("whitelist.json", JSON.stringify(whitelist));
                 }
             }
         );
