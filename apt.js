b6eaf237facd78efd4ed28fe6d934031fe4b36d5
Plenty Su <plenty.su@gmail.com>
2016-03-16 13:50:36 +0000

    Send pushover by using node-pushover

--- apt.js
+++ apt.js
@@ -4,21 +4,11 @@ var request = require("request");
 var cheerio = require("cheerio");
 var path = require("path");
 var fs = require("fs");
+var pushover = require("node-pushover");
 
 var target = "http://www.mzcan.com/taiwan/3682/irwebsite_c/index.php?mod=monthlysales";
-
-function pushover(message) {
-    var pushover = JSON.parse(fs.readFileSync("pushover.json"));
-    if (pushover.users.length && pushover.token) {
-        for (var user of pushover.users) {
-            request.post("https://api.pushover.net/1/messages.json").form({
-                token: pushover.token,
-                user: user,
-                message: message
-            });
-        }
-    }
-}
+var pushover_config = JSON.parse(fs.readFileSync("pushover.json"));
+var push = new pushover({ token: pushover_config.token });
 
 request(target, function (error, response, body) {
     if (!error) {
@@ -30,7 +20,12 @@ request(target, function (error, response, body) {
 
             var whitelist = JSON.parse(fs.readFileSync("whitelist.json"));
             if (whitelist.apt.indexOf(filename) === -1) {
-                pushover(`Asia Pacific Telecom might update a new financial statement: <${filename}>`);
+                var title = "Note";
+                var message = `Asia Pacific Telecom might update a new financial statement: <${filename}>`;
+                for (var user of pushover_config.users) {
+                    push.send(user, title, message);
+                }
+
                 whitelist.apt.push(filename);
                 fs.writeFileSync("whitelist.json", JSON.stringify(whitelist));
             }
