6241b9b360f9f76f41bc4fa7c7f1e077ed3d9894
Plenty Su <plenty.su@gmail.com>
2016-03-29 17:31:11 +0000

    Simplify apt/fetnet modules. Just return found files

--- apt.js
+++ apt.js
@@ -1,34 +1,20 @@
 "use strict";
 
-var request = require("request");
+var request = require("sync-request");
 var cheerio = require("cheerio");
 var path = require("path");
-var fs = require("fs");
-var pushover = require("node-pushover");
 
-var target = "http://www.mzcan.com/taiwan/3682/irwebsite_c/index.php?mod=monthlysales";
-var pushover_config = JSON.parse(fs.readFileSync("pushover.json"));
-var push = new pushover({ token: pushover_config.token });
+var file = "";
+var url = "http://www.mzcan.com/taiwan/3682/irwebsite_c/index.php?mod=monthlysales";
+var response = request('GET', url);
 
-request(target, function (error, response, body) {
-    if (!error) {
-        var $ = cheerio.load(body);
-        var imgs = $('img[src="img/corporate/btn_stock01.jpg"]');
-        if (imgs.length == 1) {
-            var href = imgs.first().parent().attr("href");
-            var filename = path.basename(href);
-
-            var whitelist = JSON.parse(fs.readFileSync("whitelist.json"));
-            if (whitelist.apt.indexOf(filename) === -1) {
-                var title = "Note";
-                var message = `Asia Pacific Telecom might update a new financial statement: <${filename}>`;
-                for (var user of pushover_config.users) {
-                    push.send(user, title, message);
-                }
-
-                whitelist.apt.push(filename);
-                fs.writeFileSync("whitelist.json", JSON.stringify(whitelist));
-            }
-        }
+if (response.statusCode == 200) {
+    var $ = cheerio.load(response.getBody('utf8'));
+    var imgs = $('img[src="img/corporate/btn_stock01.jpg"]');
+    if (imgs.length == 1) {
+        var href = imgs.first().parent().attr("href");
+        file = path.basename(href);
     }
-});
+}
+
+module.exports = { file: file };
--- fetnet.js
+++ fetnet.js
@@ -1,34 +1,21 @@
 "use strict";
 
-var request = require("request");
+var request = require("sync-request");
 var cheerio = require("cheerio");
 var path = require("path");
-var fs = require("fs");
-var pushover = require("node-pushover");
 
-var target = "https://www.fetnet.net/cs/Satellite/Corporate/coOperationAnalysis";
-var pushover_config = JSON.parse(fs.readFileSync("pushover.json"));
-var push = new pushover({ token: pushover_config.token });
+var file = ""
+var url = "https://www.fetnet.net/cs/Satellite/Corporate/coOperationAnalysis";
+var response = request('GET', url);
 
-request(target, function (error, response, body) {
-    if (!error) {
-        var $ = cheerio.load(body);
-        $('a[href$=".xlsx"]:contains("下載每月營運資訊")').each(
-            function (i, e) {
-                var href = $(e).attr("href");
-                var filename = path.basename(href);
-                var whitelist = JSON.parse(fs.readFileSync("whitelist.json"));
-                if (whitelist.fetnet.indexOf(filename) === -1) {
-                    var title = "Note";
-                    var message = `Far EasTone Telecom might update a new financial statement: <${filename}>`;
-                    for (var user of pushover_config.users) {
-                        push.send(user, title, message);
-                    }
+if (response.statusCode == 200) {
+    var $ = cheerio.load(response.getBody('utf8'));
+    $('a[href$=".xlsx"]:contains("下載每月營運資訊")').each(
+        function (i, e) {
+            var href = $(e).attr("href");
+            file = path.basename(href);
+        }
+    );
+}
 
-                    whitelist.fetnet.push(filename);
-                    fs.writeFileSync("whitelist.json", JSON.stringify(whitelist));
-                }
-            }
-        );
-    }
-});
+module.exports = { file: file };
